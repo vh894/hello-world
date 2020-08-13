e4259f2f62e6fea2321223e709e3cb66b371d353
vh894 <68515864+vh894@users.noreply.github.com>
2020-08-13 12:48:28 -0700

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,19 @@
+/******************************************************************************
+
+                            Online Java Compiler.
+                Code, Compile, Run and Debug java program online.
+Write your code in this editor and press "Run" button to execute it.
+
+*******************************************************************************/
+
+public class Main
+{
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+		System.out.println("My name is Vanessa Hu.");
+		System.out.println("My hobbies are watching YouTube and reading.");
+		System.out.println("I want to major in computer science.");
+		System.out.println("My favorite food are egg tarts but I don't like to eat eggs.");
+		System.out.println("My favorite color is blue.");
+	}
+}
